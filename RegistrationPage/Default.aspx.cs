f35ed92ecceb3f0425ed31a58d1e6edcfb67c532
Radu <Radu@DESKTOP-CI3J6PC>
2018-05-19 18:56:34 +0200

    Heee

--- RegistrationPage/Default.aspx.cs
+++ RegistrationPage/Default.aspx.cs
@@ -10,7 +10,7 @@ using System.Web.UI.WebControls;
 public partial class _Default : System.Web.UI.Page
 {
     SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
-
+    //hee
     protected void Page_Load(object sender, EventArgs e)
     {
 
