a03ec5d4e3a5224fdd8a4d66c7d97e7a0b8ae462
ivailotsvetkov <ivailo.tsvetkov97@gmail.com>
2018-05-26 18:25:24 +0200

    mainpage

--- RegistrationPage/LogInPage.aspx.cs
+++ RegistrationPage/LogInPage.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,6 +9,7 @@ using System.Web.UI.WebControls;
 
 public partial class LogInPage : System.Web.UI.Page
 {
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -15,11 +18,35 @@ public partial class LogInPage : System.Web.UI.Page
     protected void btnRegistration_Click(object sender, EventArgs e)
     {
         Response.Redirect("Default.aspx");
-        
+
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        wrongUser.Visible = true;
+        string uName = txtUsername.Text;
+        string password = txtPass.Text;
+
+        string queryString =
+            "SELECT Username, Password from RegistrationTable "
+                + "WHERE Username = @uName AND Password=@password ";
+        SqlCommand command = new SqlCommand(queryString, con);
+        command.Parameters.AddWithValue("@uName", uName);
+        command.Parameters.AddWithValue("@password", password);
+        con.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            if (uName == reader["Username"].ToString() & password == reader["Password"].ToString())
+            {
+
+                Response.Redirect("MainPage.aspx");
+            }
+
+        }
+        labelWrongUser.Visible = true;
+        reader.Close();
+
+
+
     }
 }
---
+++ RegistrationPage/MainPage.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MainPage.aspx.cs" Inherits="MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="grid" runat="server">
+            </asp:GridView>
+            <asp:Label ID="Label1" runat="server" Text="Type"></asp:Label>
+            <asp:TextBox ID="txtType" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Calories burned"></asp:Label>
+            <asp:TextBox ID="txtCalories" runat="server"></asp:TextBox>
+            <asp:Button ID="btnRefresh" runat="server" OnClick="btnRefresh_Click" Text="Refresh" />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Duration"></asp:Label>
+            <asp:TextBox ID="txtDuration" runat="server"></asp:TextBox>
+            <asp:Button ID="btnCreate" runat="server" OnClick="btnCreate_Click" Text="Create" />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
